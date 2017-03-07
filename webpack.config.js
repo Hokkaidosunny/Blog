d063a1f36529911c475f12bffcbd8d8d0391be8a
shenshuaijia <shenshuaijia@shenshuaijiadeMacBook-Air.local>
2017-03-07 14:56:26 +0800

    modify webpackconfig

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ function getPlugins() {
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.join(__dirname, '/build/'),
+    path: path.join(__dirname, '/dist/'),
     filename: '[name].[hash].js'
   },
   module: {
